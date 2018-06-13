78a2cb5b82b37bfd4ba81b0c77277c3bd7e333e0
Tejas Jasani <bharatappguru@gmail.com>
2018-06-13 11:21:19 -0700

    Update Files

--- use-c-delegates-in-unity/Assets/ObjectController.cs
+++ use-c-delegates-in-unity/Assets/ObjectController.cs
@@ -4,45 +4,61 @@ using UnityEngine;
 
 public class ObjectController : MonoBehaviour {
 	#region PUBLIC_VARIABLES
-	public	Renderer objectRendere;
-	public delegate void  MyDelegate();
-	MyDelegate myDelegate;
-	#endregion
-	#region UNITY_CALLBACKS
-	private void Update(){
-		if (Input.GetKeyDown (KeyCode.DownArrow)) {
-			if(myDelegate!=null)
-				myDelegate();
-			myDelegate += ChangePosition;
-			myDelegate += ChangeColor;
-			myDelegate = ChangeRotation;
-		}
-	}
-	private void OnDisable(){
-		myDelegate -= ChangePosition;
-		myDelegate -= ChangeColor;
-	}
-	#endregion
-	#region PRIVATE_FUNCTION
-	void ChangePosition(){
-		StartCoroutine (Movement (2f));
-	}
-	void ChangeColor(){
-		objectRendere.material.color = Color.yellow;
-	}
-	void ChangeRotation(){
-		transform.Rotate (0, 90, 0);
-	}
-	#endregion
-	#region CO_ROUTINE
-	IEnumerator Movement(float time){
-		float i = 0;
-		float rate = 1 / time;
-		while(i<1){
-			i += Time.deltaTime * rate;
-			transform.position = Vector3.Lerp (Vector3.zero,Vector3.up*4,i);
-			yield return null;
-		}
-	}
-	#endregion
+    public Renderer objectRendere;
+    public delegate void MyDelegate();
+    MyDelegate myDelegate;
+    #endregion
+
+    #region UNITY_CALLBACKS
+    private void OnEnable()
+    {
+        myDelegate += ChangePosition;
+        myDelegate += ChangeColor;
+        myDelegate = ChangeRotation;
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (myDelegate != null)
+                myDelegate();
+        }
+    }
+    private void OnDisable()
+    {
+        myDelegate -= ChangePosition;
+        myDelegate -= ChangeColor;
+    }
+    #endregion
+
+    #region PRIVATE_FUNCTION
+    void ChangePosition()
+    {
+        StartCoroutine(Movement(2f));
+    }
+    void ChangeColor()
+    {
+        objectRendere.material.color = Color.yellow;
+    }
+    void ChangeRotation()
+    {
+        transform.Rotate(0, 90, 0);
+    }
+    #endregion
+
+    #region CO_ROUTINE
+    IEnumerator Movement(float time)
+    {
+        float i = 0;
+        float rate = 1 / time;
+        while (i < 1)
+        {
+            i += Time.deltaTime * rate;
+            transform.position = Vector3.Lerp(Vector3.zero, Vector3.up * 4, i);
+            yield return null;
+        }
+    }
+    #endregion
+
 }
